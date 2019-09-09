c1eb0b02632e4986c1cdd5ef71247ba21bcb2b6e
J-B Garway-Templeman <jbgarwayt@gmail.com>
2019-09-09 11:03:57 +0100

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,5 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
+
   def change
     create_table :artists do |t|
       t.string :name
@@ -7,4 +8,5 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.string :hometown
     end
   end
+
 end
